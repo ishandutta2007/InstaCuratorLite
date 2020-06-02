b39bd94d6664b78b3a92e1e7d229d3cc978c604f
Ishan Dutta <ishandutta2007@gmail.com>
2020-06-02 11:54:06 +0530

    run_all

---
+++ run_all.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+$DIR/venv/bin/python $DIR/instadownload.py -du smrithi.mandhana.fc.new -su smrithi.mandhana.fc,smritivirat18,smritimandhana__,18_smriti_18 
